3e5376a81e703691d2f8d01a855ca3b2ca3479d5
Micah Lindley <micahincorporated@gmail.com>
2022-08-26 13:38:33 -0500

    Remove all of aetinx's trolling
    This reverts commit 79bd01a2acac8af9f60cf67a53366f03d383c891.

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-This site was a project by Sargent Coding, and is dead now. Please visit [our site](https://saco.dev), it's fucking awesome!
+# The Scratch Informant
 
-Uh-oh, did I just write the word "fucking"? Now Micah's gonna cry because I wrote a swear word! Boo-hoo, get a life.
+The Informant is a news source for the [Scratch](https://scratch.mit.edu/) community.  It ~~is~~ will be written by various authors around the website and ~~is~~ will be powered by [Vercel](https://vercel.com)'s serverless functions.
+
+~~Currently only the landing page is active, [here](https://informant.micahlindley.com).~~ It's active now!
+
+### Support me
+
+If you want to support me, you can buy me a coffee:
+
+[![Buy Me A Coffee](https://cdn.buymeacoffee.com/buttons/lato-red.png)](https://buymeacoff.ee/micahlt)
---
+++ home.js
@@ -0,0 +1,64 @@
+let db = firebase.firestore();
+let postOffset = 1;
+
+window.addEventListener('load', renderStories);
+
+function renderStories() {
+  let mainData = db.collection("data").doc("main");
+  mainData.get().then(function(doc) {
+    if (doc.exists) {
+      db.collection("posts").where("Canonical", "==", doc.data().canon).get().then(function(querySnapshot) {
+        console.log(querySnapshot);
+        querySnapshot.forEach(function(docc) {
+          document.getElementById("featured-link").href = "read.html?s=" + docc.id;
+          document.getElementById("featured-img").src = docc.data().Image;
+          document.getElementById("featured-title").innerText = docc.data().Title;
+          document.getElementById("featured-subtitle").innerText = docc.data().Subtitle;
+        })
+        for (let i = postOffset; i < postOffset + 8; i++) {
+          generatePost(doc.data().canon - i);
+        }
+      })
+    } else {
+      window.location.reload();
+    }
+  }).catch(function(error) {
+    console.error("Error getting data:", error);
+  });
+}
+
+function generatePost(canonNum) {
+  let post = document.createElement("a");
+  let image = document.createElement("img");
+  let imageContainer = document.createElement("div");
+  let title = document.createElement("h2");
+  let subtitle = document.createElement("p");
+  let articleRef = db.collection("posts").where("Canonical", "==", canonNum);
+  articleRef.get().then(function(querySnapshot) {
+    if (querySnapshot.empty) {
+      document.getElementById("showMore").style.display = "none";
+    }
+    console.log(querySnapshot.empty)
+    querySnapshot.forEach(function(doc) {
+      post.href = "read.html?s=" + doc.id;
+      post.classList.add("post")
+      image.src = doc.data().Image;
+      title.innerText = doc.data().Title;
+      subtitle.innerText = doc.data().Subtitle;
+      imageContainer.classList.add("post-img-container");
+      imageContainer.appendChild(image);
+      post.appendChild(imageContainer);
+      post.appendChild(title);
+      post.appendChild(subtitle);
+      document.getElementsByClassName("other-posts")[0].appendChild(post);
+      postOffset++;
+    })
+  }).catch(function(error) {
+    console.error("Error getting post:", error);
+  })
+}
+
+document.getElementById("showMore").addEventListener("click", function(event) {
+  event.preventDefault();
+  renderStories();
+});
